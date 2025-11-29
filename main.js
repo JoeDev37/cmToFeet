434457a07fb653380286ceacdee9528d1b75a88f
JoeDev37 <alonejoe367@gmail.com>
2025-11-29 18:09:06 +0300

    cm to feet(coding)

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 
-console.log('Hello, World')
+let heightInCm = prompt('Wat is your height?');
+
+let toInche = heightInCm/2.54;
+
+let toFeet = Math.floor(toInche/12);
+let remainInches = Math.round(toInche % 12)
+
+console.log(`${toFeet} feet and ${remainInches} inches` );
+console.log(`${toFeet}'${remainInches}`)
+
